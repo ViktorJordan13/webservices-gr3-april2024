99f66f1a948be28530d92aee83058bca7b12f2ea
Viktor Jordanoski <viktorj560@gmail.com>
2024-04-16 21:57:13 +0200

    feat: Class 3 end

--- c3/handlers/cars.js
+++ c3/handlers/cars.js
@@ -10,10 +10,75 @@ const {
 // handlerite (fajlov kade sto se naogame sega i go pisuvame ova) - tuka gi koristime lokalnite funkcii od pkg/cars/index.js
 // (ovie sto gi importirame najgore) za da manipuliraat so cars.json ( preku req i res )
 
-// Task
-// Podsetete se od Node.js i MongoDB modulite kako se pravea handlers (vo niv koristevme MVC design pattern, pa tamu ni bea controllers)
-// i napravete gi slednive handler funkcii koi ke gi povikuvaat funkciite koi gi importiravme od pkg/cars
 // SOLID 
 // S - single responsibility
+
+
+// Task
+// Podsetete se od Node.js i MongoDB modulite kako se pravea handlers (vo niv koristevme MVC design pattern,
+// pa tamu ni bea controllers) i napravete gi slednive handler funkcii koi ke gi povikuvaat funkciite 
+// koi gi importiravme od pkg/cars
 // Napravete gi slednive handler funkcii: getAll, getById, create, update, remove
-// exportirajte gi i importirajte gi vo glavniot vlez na aplikacijata index.js (tamu kade sto ke go definirame express)
+// exportirajte gi i importirajte gi vo glavniot vlez na aplikacijata index.js 
+// (tamu kade sto ke go definirame express)
+
+const getAll = async(req, res) => {
+    try{
+        const cars = await getAllCars();
+        return res.status(200).send(cars);
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+const getById = async(req, res) => {
+    try{
+        const id = Number(req.params.id);
+        const car = await getCarById(id);
+        return res.status(200).send(car);
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+const create = async(req, res) => {
+    try{
+        await addCar(req.body);
+        return res.status(201).send(req.body);
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+const update = async(req, res) => {
+    try{
+        const id = Number(req.params.id);
+        await updateCar(id, req.body);
+        return res.status(204).send("") // Succes(OK) but no entity body
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+const remove = async(req, res) => {
+    try{
+        const id = Number(req.params.id);
+        await removeCar(id);
+        return res.status(200).send("Car deleted!");
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
--- c3/index.js
+++ c3/index.js
@@ -6,9 +6,33 @@
 // pkg/cars komunicira so pkg/files i so handlers
 // handler komunicira so index.js (ovoj fajl kade sto e ova celoto napisano i fajlot kade sto go definirame express)
 
+const express = require("express");
 const api = express();
 
 api.use(express.json());
 
+const {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+} = require ("./handlers/cars");
+
 //GET
-api.get("/cars", );
+api.get("/cars", getAll);
+api.get("/cars/:id", getById);
+//POST
+api.post("/cars", create);
+//PUT
+//za ovaa logika na ovoj cas nema da ni treba put ruta, bidejki koristime patch vo logikata
+//PATCH
+api.patch("/cars/:id", update);
+//DELETE
+api.delete("/cars/:id", remove);
+
+api.listen(10000, (err) => {
+    err ? console.error(err) : console.log("Server started at port 10000!");
+});
+
+
